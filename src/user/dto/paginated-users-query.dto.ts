1faae2545eaee41cdeb95f9a95af6c3366e944ee
kenibeck <deyler_mena69222@elpoli.edu.co>
2026-01-21 02:26:06 -0500

    feat: enhance user pagination and role management with improved filters and validation

--- src/user/dto/paginated-users-query.dto.ts
+++ src/user/dto/paginated-users-query.dto.ts
@@ -1,5 +1,5 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { Type } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 import { IsBoolean, IsInt, IsOptional, IsString, Min } from 'class-validator';
 
 export class PaginatedUsersQueryDto {
@@ -65,7 +65,12 @@ export class PaginatedUsersQueryDto {
     example: true,
   })
   @IsOptional()
-  @Type(() => Boolean)
+  @Transform(({ value }) => {
+    if (value === 'true') return true;
+    if (value === 'false') return false;
+    if (typeof value === 'boolean') return value;
+    return undefined;
+  })
   @IsBoolean()
   isActive?: boolean;
 }
